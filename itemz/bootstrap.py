10197bfb2d5378689f037d993c663b44ece2b282
jererc <jererc@gmail.com>
2024-11-12 14:44:17 +0400

    update

--- itemz/bootstrap.py
+++ itemz/bootstrap.py
@@ -6,6 +6,7 @@ from svcutils import Bootstrapper
 Bootstrapper(
     script_path=os.path.join(os.path.dirname(os.path.realpath(__file__)),
         'itemz.py'),
+    crontab_schedule='*/2 * * * *',
     linux_args=['--task'],
     windows_args=['--daemon'],
 ).run()
