ef8b93c71f13716daf627a280c9fd6e536e34068
jererc <jererc@gmail.com>
2024-11-15 15:40:48 +0400

    update

--- itemz/bootstrap.py
+++ itemz/bootstrap.py
@@ -7,5 +7,5 @@ Bootstrapper(
     script_path=os.path.join(os.path.dirname(os.path.realpath(__file__)),
         'itemz.py'),
     linux_args=['--task'],
-    windows_args=['--daemon'],
+    windows_args=['--task'],
 ).run()
